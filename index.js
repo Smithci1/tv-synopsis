bf8ee49070b2fee2ded3865783f5e24772ac05c1
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-25 15:52:14 -0400

    added a 404 template from  google

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const showdata = require('./showdata')
 app.set('view engine', 'pug')
 app.use(express.static('views'))
 app.get('/', (req,res) => {
-    res.render('index')
+    res.render('index', {showdata})
 })
+
 app.all('*',(req,res) =>{
-    return res.status(404).send('ummm.. where do you think your going??')
+    return res.status(404).sendFile
+    (path.join(__dirname + '/404.html'))
 })
 
 app.listen(3370, () =>{
